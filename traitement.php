b60e2135af2cd26dcbb6cb66a3b0915dd6534b8d
Germainlc <120478543+Germainlc@users.noreply.github.com>
2022-12-13 15:20:12 +0100

    Update traitement.php

--- traitement.php
+++ traitement.php
@@ -1,10 +1,14 @@
 <?php
+header('Content-Type: text/plain');
  
-header("Content-Type: text/plain"); // Utilisation d'un header pour spécifier le type de contenu de la page. Ici, il s'agit juste de texte brut (text/plain). 
- 
-$variable1 = (isset($_GET["variable1"])) ? $_GET["variable1"] : NULL;
-   $inF = fopen("ip.txt","a");
-fputs($inF,$variable1."\n");
-fclose($inF);
- 
+$variable1 = isset($_GET['variable1']) ? $_GET['variable1'] : null;
+if ($variable1) {
+  $inF = fopen('ip.txt', 'a');
+  fputs($inF, $variable1. "\n");
+  fclose($inF);
+  echo 'Inscription de l’IP réussie';
+} else {
+  echo 'Information manquante dans la requête';
+}
+
 ?>
